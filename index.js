b22d204f935e274e40dc66c8863be2dcc18a53c6
Begotten-Traveler <solomondev002@gmail.com>
2025-04-09 23:16:39 +0100

    add: JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Yo! I'm properly linked");
+let fruits = ["🍎", "🍊", "🍎", "🍊", "🍎", "🍊", "🍎", "🍊", "🍎", "🍊"];
+let appleShelf = document.getElementById("appleshelf");
+let orangeShelf = document.getElementById("orangeshelf");
+
+
+function fruitSorting(){
+    for (let i = 0; i < fruits.length; i++){
+        if (fruits[i] === "🍎"){
+            appleShelf.textContent += "🍎";
+        } else if (fruits[i] === "🍊"){
+            orangeShelf.textContent += "🍊";
+        }
+    }
+}
+
+fruitSorting();
